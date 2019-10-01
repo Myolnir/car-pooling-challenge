318582d4c0c82f64b8116b1a869a5eebead79f64
angel <angel.muelas@denizen.io>
2019-10-01 13:17:02 +0200

    added embbed mongo to be able to pass de acceptance tests..

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const restify = require('restify');
 const restifyPlugins = require('restify-plugins');
 const route_catalog = require('./routes/index');
 const logger = require('./util/logger');
+const mongo = require('mongo-unit');
 
 
 const server = restify.createServer({
@@ -20,8 +21,12 @@ server.use(restifyPlugins.urlEncodedBodyParser());
 server.use(restifyPlugins.acceptParser(server.acceptable));
 server.use(restifyPlugins.queryParser());
 server.use(restifyPlugins.fullResponse());
+
 server.listen(config.port, () => {
   logger.debug('test logger');
+  mongo.start({dbName: 'car_pooling'}).then(url => {
+    console.log('mongo is started: ', url);
+  });
   console.log('%s listening at %s', server.name, server.address().port);
   route_catalog(server);
 });
